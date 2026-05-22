cd19428553af3823defe698882df4b90e1b66978
VINICIUS506 <viny.fsm@gmail.com>
2026-05-22 21:28:11 +0000

    Updated

---
+++ variables/app.py
@@ -0,0 +1,27 @@
+students_count = 1000
+rating = 4.99
+is_published = False
+course_name = "Python Programming"
+
+# Strings
+print(len(course_name))
+print(course_name[0])
+print(course_name[-1])
+print(course_name[0:6])
+print(course_name[:3])
+print(course_name[:])
+first = "Mosh"
+last = "Hamedani"
+print(f"{first} {last}")
+print(course_name.upper())
+print("Pro" in course_name)
+
+
+# Numbers
+print(10 + 3)
+print(10 - 3)
+print(10 * 3)
+print(10 / 3)
+print(10 // 3)
+print(10 % 3)
+print(10 ** 3)
